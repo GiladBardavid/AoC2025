f7145face3dfe995578bb9bd4040d683ddb1e48c
GiladBardavid <gilad@bardavid.com>
2025-12-13 21:54:01 +0200

    finished day4 task1

--- Day4/task1.cpp
+++ Day4/task1.cpp
@@ -17,8 +17,41 @@ using namespace std;
 #define pb push_back
 #define vi vector<int>
 
-void solve() {
+const char TARGET = '@';
 
+int valid(vector<string> s, int i, int j) {
+    int good = 0;
+
+    if (i > 0 && s[i - 1][j] == TARGET) good++;
+    if (i < s.size() - 1 && s[i + 1][j] == TARGET) good++;
+    if (j > 0 && s[i][j - 1] == TARGET) good++;
+    if (j < s[0].length() - 1 && s[i][j + 1] == TARGET) good++;
+
+    if (i > 0 && j > 0 && s[i - 1][j - 1] == TARGET) good++;
+    if (i < s.size() - 1 && j > 0 && s[i + 1][j - 1] == TARGET) good++;
+    if (i > 0 && j < s[0].length() - 1 && s[i - 1][j + 1] == TARGET) good++;
+    if (i < s.size() - 1 && j < s[0].length() - 1 && s[i + 1][j + 1] == TARGET) good++;
+
+    return good < 4;
+}
+
+
+void solve() {
+    vector<string> input;
+    string row;
+    while (cin >> row) {
+        input.push_back(row);
+    }
+
+    int res = 0;
+    for (int i = 0; i < input.size(); i++) {
+        for (int j = 0; j < input[0].length(); j++) {
+            if (input[i][j] == TARGET) {
+                res += valid(input, i, j);
+            }
+        }
+    }
+    cout << res << endl;
 }
 
 
