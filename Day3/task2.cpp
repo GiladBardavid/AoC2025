e067384ba6002aef720dbff393c59b5dad3149fa
GiladBardavid <gilad@bardavid.com>
2025-12-13 21:29:49 +0200

    finished d3t2

--- Day3/task2.cpp
+++ Day3/task2.cpp
@@ -20,66 +20,23 @@ using namespace std;
 const int LENGTH_LIMIT = 12;
 
 int solve_row(string s) {
-    /*
-     * Idea: greedy over digits from highest to lowest. Then we have the digits of the resulting 12 digit number.
-     * For all digits other than the smallest one we choose as part of out 12, we just take them all.
-     * Then for the smallest one we need to loop from right to left and take greedily while every time we pass a larger
-     * digit we keep track of that then insert the small digit into the right position in the resulting integer.
-     */
-    int digit_to_occurrences_in_largest[10] = {0};
-    int smallest_included_digit = 9;
-    int included_digits = 0;
-    for (int i = 9; i >= 0; i--) {
-        for (char c : s) {
-            if (c - '0' == i) {
-                digit_to_occurrences_in_largest[i]++;
-                included_digits++;
-                smallest_included_digit = i;
-            }
-            if (included_digits == LENGTH_LIMIT) {
-                break;
-            }
-        }
-        if (included_digits == LENGTH_LIMIT) {
-            break;
-        }
-    }
-
-    cout << "smallest digit = " << smallest_included_digit << endl;
-    cout << "occurrences of it = " << digit_to_occurrences_in_largest[smallest_included_digit] << endl;
-
-    int smallest_digit_occurrences = 0;
-    int res_digits[LENGTH_LIMIT] = {0};
-    int idx_in_res = 0;
-
-    for (int i = s.length() - 1; i >= 0; i--) {
-        int curr = s[i] - '0';
-        if (curr < smallest_included_digit) {
-            continue;
-        }
-
-        if (curr > smallest_included_digit) {
-            res_digits[idx_in_res] = curr;
-            idx_in_res++;
-            continue;
-        }
-
-        // curr == smallest_included_digit
-        if (smallest_digit_occurrences < digit_to_occurrences_in_largest[smallest_included_digit]) {
-            res_digits[idx_in_res] = curr;
-            idx_in_res++;
-            smallest_digit_occurrences++;
-        }
-    }
-
     int res = 0;
-    for (int i = LENGTH_LIMIT - 1; i >= 0; i--) {
+    int l = 0;
+    for (int r = s.length() - LENGTH_LIMIT; r < s.length(); r++) {
+        int mx = -1;
+        int idx_of_first_max = 0;
+        for (int i = l; i <= r; i++) {
+            int cur = s[i] - '0';
+            if (cur > mx) {
+                mx = cur;
+                idx_of_first_max = i;
+            }
+        }
+
         res *= 10;
-        res += res_digits[i];
+        res += mx;
+        l = idx_of_first_max + 1;
     }
-
-    cout << "row res = " << res << endl;
-
     return res;
 }
 
